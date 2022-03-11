edbe3616c2d0e747c7ad7c030d0c86ae5007d06c
HollyT85 <h.e.turner@gmail.com>
2022-03-11 10:35:43 +0000

    created sales columns

--- run.py
+++ run.py
@@ -78,6 +78,22 @@ def calculate_surplus_data(sales_row):
     return surplus_data
 
 
+def get_last_5_entries_sales():
+    """
+    collect sales entries for the last 5 days
+    """
+    sales=SHEET.worksheet('sales')
+
+    columns=[]
+    for ind in range(1, 7):
+        column=sales.col_values(ind)
+        columns.append(column[-5:])
+    
+    return columns
+
+
+
+
 def main():
     """
     Runs all functions
@@ -89,4 +105,6 @@ def main():
     update_worksheet(new_surplus_data, "surplus")
 
 print("Welcome to Love Sandwiches data automation\n")
-main()
+#main()
+
+sales_columns = get_last_5_entries_sales()
